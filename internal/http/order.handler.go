880cfe95c3759f25f5e5e7eecf396a5157857e00
rulanugrh <nextarituenak01@gmail.com>
2023-09-30 00:13:26 +0700

    feat: order handler

--- internal/http/order.handler.go
+++ internal/http/order.handler.go
@@ -1,8 +1,14 @@
 package handler
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
+	"strconv"
 
+	"github.com/gorilla/mux"
+	"github.com/rulanugrh/uranus/internal/domain/entity"
+	"github.com/rulanugrh/uranus/internal/domain/web"
 	portHandler "github.com/rulanugrh/uranus/internal/http/port"
 	"github.com/rulanugrh/uranus/internal/service/port"
 )
@@ -17,6 +23,59 @@ func NewOrderHandler(serv port.OrderInterfaceService) portHandler.OrderInterface
 	}
 }
 
-func(hnd *orderhandler) CreateOrder(w http.ResponseWriter, r *http.Request) {}
+func (hnd *orderhandler) CreateOrder(w http.ResponseWriter, r *http.Request) {
+	var req entity.Order
+	data, _ := ioutil.ReadAll(r.Body)
 
-func(hnd *orderhandler) FindByID(w http.ResponseWriter, r *http.Request) {}
+	json.Unmarshal(data, &req)
+	result, err := hnd.service.CreateOrder(req)
+	if err != nil {
+		res := web.ResponseFailure{
+			Code:    http.StatusBadRequest,
+			Message: "Cant create order",
+		}
+
+		response, _ := json.Marshal(res)
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(response)
+	}
+
+	res := web.ResponseSuccess{
+		Code:    200,
+		Message: "Success create order",
+		Data:    result,
+	}
+
+	response, _ := json.Marshal(res)
+	w.WriteHeader(http.StatusOK)
+	w.Write(response)
+}
+
+func (hnd *orderhandler) FindByID(w http.ResponseWriter, r *http.Request) {
+	getID := mux.Vars(r)
+	paramsID := getID["id"]
+
+	id, _ := strconv.Atoi(paramsID)
+
+	result, err := hnd.service.FindByID(uint(id))
+	if err != nil {
+		res := web.ResponseFailure{
+			Code:    http.StatusBadRequest,
+			Message: "Cant find order by this id",
+		}
+
+		response, _ := json.Marshal(res)
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(response)
+	}
+
+	res := web.ResponseSuccess{
+		Code:    http.StatusOK,
+		Message: "Success find order",
+		Data:    result,
+	}
+
+	response, _ := json.Marshal(res)
+	w.WriteHeader(http.StatusOK)
+	w.Write(response)
+}
