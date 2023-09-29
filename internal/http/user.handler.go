a70d9fb3a4070223647d6d55426321583647663a
rulanugrh <nextarituenak01@gmail.com>
2023-09-30 00:15:30 +0700

    feat: user handler

--- internal/http/user.handler.go
+++ internal/http/user.handler.go
@@ -1,12 +1,17 @@
 package handler
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
+	"strconv"
 
+	"github.com/gorilla/mux"
+	"github.com/rulanugrh/uranus/internal/domain/entity"
+	"github.com/rulanugrh/uranus/internal/domain/web"
 	portHandler "github.com/rulanugrh/uranus/internal/http/port"
 	"github.com/rulanugrh/uranus/internal/service/port"
 )
-
 
 type userhandler struct {
 	service port.UserInterfaceService
@@ -18,10 +23,122 @@ func NewUserHandler(serv port.UserInterfaceService) portHandler.UserInterfaceHTT
 	}
 }
 
-func(hnd *userhandler) CreateUser(w http.ResponseWriter, r *http.Request) {}
+func (hnd *userhandler) CreateUser(w http.ResponseWriter, r *http.Request) {
+	var req entity.User
+	data, _ := ioutil.ReadAll(r.Body)
 
-func(hnd *userhandler) FindByID(w http.ResponseWriter, r *http.Request) {}
+	json.Unmarshal(data, &req)
+	result, err := hnd.service.CreateUser(req)
+	if err != nil {
+		res := web.ResponseFailure{
+			Code:    http.StatusBadRequest,
+			Message: "Cant create user",
+		}
 
-func(hnd *userhandler) Update(w http.ResponseWriter, r *http.Request) {}
+		response, _ := json.Marshal(res)
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(response)
+	}
 
-func(hnd *userhandler) Delete(w http.ResponseWriter, r *http.Request) {}
+	res := web.ResponseSuccess{
+		Code:    200,
+		Message: "Success create user",
+		Data:    result,
+	}
+
+	response, _ := json.Marshal(res)
+	w.WriteHeader(http.StatusOK)
+	w.Write(response)
+}
+
+func (hnd *userhandler) FindByID(w http.ResponseWriter, r *http.Request) {
+	getID := mux.Vars(r)
+	paramsID := getID["id"]
+
+	id, _ := strconv.Atoi(paramsID)
+
+	result, err := hnd.service.FindByID(uint(id))
+	if err != nil {
+		res := web.ResponseFailure{
+			Code:    http.StatusBadRequest,
+			Message: "Cant find user by this id",
+		}
+
+		response, _ := json.Marshal(res)
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(response)
+	}
+
+	res := web.ResponseSuccess{
+		Code:    http.StatusOK,
+		Message: "Success find user",
+		Data:    result,
+	}
+
+	response, _ := json.Marshal(res)
+	w.WriteHeader(http.StatusOK)
+	w.Write(response)
+}
+
+func (hnd *userhandler) Update(w http.ResponseWriter, r *http.Request) {
+	var req entity.User
+	data, _ := ioutil.ReadAll(r.Body)
+
+	json.Unmarshal(data, &req)
+
+	getID := mux.Vars(r)
+	paramsID := getID["id"]
+
+	id, _ := strconv.Atoi(paramsID)
+
+	result, err := hnd.service.UpdateUser(uint(id), req)
+	if err != nil {
+		res := web.ResponseFailure{
+			Code:    http.StatusBadRequest,
+			Message: "Cant update user by this id",
+		}
+
+		response, _ := json.Marshal(res)
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(response)
+	}
+
+	res := web.ResponseSuccess{
+		Code:    http.StatusOK,
+		Message: "Success update user",
+		Data:    result,
+	}
+
+	response, _ := json.Marshal(res)
+	w.WriteHeader(http.StatusOK)
+	w.Write(response)
+}
+
+func (hnd *userhandler) Delete(w http.ResponseWriter, r *http.Request) {
+	getID := mux.Vars(r)
+	paramsID := getID["id"]
+
+	id, _ := strconv.Atoi(paramsID)
+
+	err := hnd.service.DeleteUser(uint(id))
+	if err != nil {
+		res := web.ResponseFailure{
+			Code:    http.StatusBadRequest,
+			Message: "Cant delete user by this id",
+		}
+
+		response, _ := json.Marshal(res)
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(response)
+	}
+
+	res := web.ResponseSuccess{
+		Code:    http.StatusOK,
+		Message: "Success delete user",
+		Data:    "Data success delete",
+	}
+
+	response, _ := json.Marshal(res)
+	w.WriteHeader(http.StatusOK)
+	w.Write(response)
+}
