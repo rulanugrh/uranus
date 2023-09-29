5408b93f6d7f129bd183fc191c6ff75c918898b7
rulanugrh <nextarituenak01@gmail.com>
2023-09-29 23:59:56 +0700

    feat: category handler

--- internal/http/category.handler.go
+++ internal/http/category.handler.go
@@ -1,8 +1,14 @@
 package handler
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
+	"strconv"
 
+	"github.com/gorilla/mux"
+	"github.com/rulanugrh/uranus/internal/domain/entity"
+	"github.com/rulanugrh/uranus/internal/domain/web"
 	portHandler "github.com/rulanugrh/uranus/internal/http/port"
 	"github.com/rulanugrh/uranus/internal/service/port"
 )
@@ -17,8 +23,83 @@ func NewCategoryHandler(serv port.CategoryInterfaceService) portHandler.Category
 	}
 }
 
-func(hnd *categoryhandler) CreateCategory(w http.ResponseWriter, r *http.Request) {}
+func (hnd *categoryhandler) CreateCategory(w http.ResponseWriter, r *http.Request) {
+	var req entity.Category
+	data, _ := ioutil.ReadAll(r.Body)
 
-func(hnd *categoryhandler) FindByID(w http.ResponseWriter, r *http.Request) {}
+	json.Unmarshal(data, &req)
+	result, err := hnd.service.CreateCategory(req)
+	if err != nil {
+		res := web.ResponseFailure{
+			Code:    http.StatusBadRequest,
+			Message: "Cant create category",
+		}
 
-func(hnd *categoryhandler) FindAll(w http.ResponseWriter, r *http.Request) {}
+		response, _ := json.Marshal(res)
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(response)
+	}
+
+	res := web.ResponseSuccess{
+		Code:    200,
+		Message: "Success create category",
+		Data:    result,
+	}
+
+	response, _ := json.Marshal(res)
+	w.WriteHeader(http.StatusOK)
+	w.Write(response)
+}
+
+func (hnd *categoryhandler) FindByID(w http.ResponseWriter, r *http.Request) {
+	getID := mux.Vars(r)
+	paramsID := getID["id"]
+
+	id, _ := strconv.Atoi(paramsID)
+
+	result, err := hnd.service.FindByID(uint(id))
+	if err != nil {
+		res := web.ResponseFailure{
+			Code:    http.StatusBadRequest,
+			Message: "Cant find category by this id",
+		}
+
+		response, _ := json.Marshal(res)
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(response)
+	}
+
+	res := web.ResponseSuccess{
+		Code:    http.StatusOK,
+		Message: "Success find category",
+		Data:    result,
+	}
+
+	response, _ := json.Marshal(res)
+	w.WriteHeader(http.StatusOK)
+	w.Write(response)
+}
+
+func (hnd *categoryhandler) FindAll(w http.ResponseWriter, r *http.Request) {
+	result, err := hnd.service.FindAll()
+	if err != nil {
+		res := web.ResponseFailure{
+			Code:    http.StatusBadRequest,
+			Message: "Cant find category",
+		}
+
+		response, _ := json.Marshal(res)
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(response)
+	}
+
+	res := web.ResponseSuccess{
+		Code:    http.StatusOK,
+		Message: "Success find category",
+		Data:    result,
+	}
+
+	response, _ := json.Marshal(res)
+	w.WriteHeader(http.StatusOK)
+	w.Write(response)
+}
